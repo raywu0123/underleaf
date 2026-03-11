13050b8fb2f57c00999ccd8ae90d821721ad4aaf
Ray Wu <raywu@Mac.home>
2026-03-11 22:51:14 +0800

    chore: add and configure eslint for the backend server

---
+++ apps/server/eslint.config.mjs
@@ -0,0 +1,23 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  {
+    ignores: ['coverage'],
+  },
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.jest
+      }
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off"
+    }
+  }
+];
